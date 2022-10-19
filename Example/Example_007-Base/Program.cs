370a290081ba1e04d96b5edb4281821ba592d44c
ys-off <yuriy.safronoff@yandex.ru>
2022-10-19 23:03:24 +0300

    Example_007

---
+++ Example/Example_007-Base/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 5;
+int b = 6;
+int c = 33;
+int d = 8;
+int e = 9;
+
+int max = a;
+
+//if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
