43b819df21298bcb70a67b8b0cb64810964edb7b
ys-off <yuriy.safronoff@yandex.ru>
2022-10-24 00:29:21 +0300

    Example_12

---
+++ Example/Example_12-MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 25, 69, 53, 28, 9, 71, 60, 96, 81, 9, 56, 14, 9, 54, 87 };
+
+int n = array.Length;
+
+int find = 9;
+
+int index = 0;
+
+while (index < n)
+{
+	if (array[index] == find)
+	{
+		Console.WriteLine(index);
+		break;
+	}
+	index++;
+}
